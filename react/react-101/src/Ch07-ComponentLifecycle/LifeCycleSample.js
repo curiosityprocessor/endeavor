af573c9144630cb68810db0ed248446488c62a1f
curiosityprocessor <curiosityprocessor@gmail.com>
2021-12-29 21:55:23 +0900

    Feat: add lifecycle sample

---
+++ react/react-101/src/Ch07-ComponentLifecycle/LifeCycleSample.js
@@ -0,0 +1,62 @@
+import { Component } from 'react';
+
+class LifeCycleSample extends Component {
+    state = {
+        number: 0,
+        color: null
+    }
+
+    myRef = null;
+
+    constructor(props) {
+        super(props);
+        console.log('constructor');
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        console.log('getDerivedStateFromProps')
+        if(nextProps.color !== prevState.color) {
+            return { color: nextProps.color}
+        }
+        return null;
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate', nextProps, nextState);
+        return nextState.number % 10 !== 4;
+    }
+
+    componentWillMount(){
+        console.log('componentWillMount');
+    }
+
+    handleClick = () => {
+        this.setState({
+            number: this.state.number + 1
+        })
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('getSnapshotBeforeUpdate');
+        if(prevProps.color !== this.props.color) {
+            return this.myRef.style.color;
+        }
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapShot) {
+        console.log('componentDidUpdate');
+        if(snapShot) {
+            console.log(`color before update: ${snapShot}`)
+        }
+    }
+
+    render() {
+        console.log('render');
+    }
+
+}
