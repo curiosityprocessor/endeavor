19c10dad449c8711330075b38b5514c8966a7138
curiosityprocessor <curiosityprocessor@gmail.com>
2022-01-12 18:37:15 +0900

    Feat: Add TodoListItem & functions

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -1,10 +1,37 @@
+import {useState} from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert'
+import TodoList from './components/TodoList';
 
 function App() {
-  return (
-    <TodoTemplate>Todo App! asddqergergw</TodoTemplate>
-  );
+    const [todos, setTodos] = useState(
+        [
+            {
+                id: 1,
+                text: 'React 101',
+                checked: true,
+            },
+            {
+                id: 2,
+                text: 'React Component Styling',
+                checked: true,
+            },
+            {
+                id: 3,
+                text: 'Todo App',
+                checked: false,
+            },
+
+        ]
+    );
+
+    return (
+        <TodoTemplate>
+            <TodoInsert />
+            <TodoList todos={todos}/>
+        </TodoTemplate>
+    );
 }
 
 export default App;
