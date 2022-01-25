c3e85c7c3235e14ca71b30ddef517935832d81a2
curiosityprocessor <curiosityprocessor@gmail.com>
2022-01-25 22:14:50 +0900

    add redux with react

--- react/redux-react/src/App.js
+++ react/redux-react/src/App.js
@@ -1,11 +1,15 @@
-import Counter from "./components/Counter";
+import React from 'react';
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 
 const App = () => {
-    return (
-        <div>
-            <Counter number={0} />
-        </div>
-    )
+  return (
+    <div>
+      <CounterContainer />
+      <hr />
+      <TodosContainer />
+    </div>
+  );
 };
 
 export default App;
