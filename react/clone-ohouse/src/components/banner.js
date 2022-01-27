ddab1e6cfdfa726a524ba65fa91cdf35e9dd34cb
curiosityprocessor <curiosityprocessor@gmail.com>
2022-01-27 22:36:02 +0900

    feat adding banner

---
+++ react/clone-ohouse/src/components/banner.js
@@ -0,0 +1,25 @@
+const banner = () => {
+    return(
+        <div className="carousel">
+            <div className="carousel__list-wrap">
+                <div className="carousel__list">
+                    <div className="carousel__list__entry">
+                        <link></link>
+                        <div className="carousel-banner">
+                            <div className="carousel-banner__content container">
+                                <div className="featured-content__cover-image">
+                                    <img />
+                                </div>
+                                <div className="featured-content__text-wrap featured-content__text-wrap--center">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default banner;
+
